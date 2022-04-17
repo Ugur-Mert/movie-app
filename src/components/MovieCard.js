86b01a872fb594f9ccafe39294d8747b0ecdf8c8
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-04-17 17:32:45 +0300

    responsive design

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 export default function MovieCard(props) {
+  function cardDetail() {
+    console.log("clicked");
+  }
+
   return (
-    <div className="movie-card">
+    <div onClick={cardDetail} className="movie-card">
       <h3 className="movie-title">{props.title}</h3>
-      <img className="movie-img" src={props.poster} />
+      <img className="movie-img" src={props.poster} alt="poster" />
       <p className="movie-overview">{props.overview.slice(0, 140) + "..."}</p>
       <div className="card-detail">
         <p>Release Date: {props.release}</p>
