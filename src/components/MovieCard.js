927e5e05fc77f17d7c2b3be89ce36aebbcb62bf1
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-04-17 17:02:15 +0300

    added some style

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,11 +3,13 @@ import React from "react";
 export default function MovieCard(props) {
   return (
     <div className="movie-card">
-      <h3>{props.title}</h3>
+      <h3 className="movie-title">{props.title}</h3>
       <img className="movie-img" src={props.poster} />
-      <p className="movie-overview">{props.overview}</p>
-      <p>{props.release}</p>
-      <p>{props.vote}</p>
+      <p className="movie-overview">{props.overview.slice(0, 140) + "..."}</p>
+      <div className="card-detail">
+        <p>Release Date: {props.release}</p>
+        <p>Rate: {props.vote}</p>
+      </div>
     </div>
   );
 }
